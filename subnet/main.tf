4cd5a6a4736bde0855c69e6873a5c1fc98a6c37a
Mavluda <mavluda.kambarova@gmail.com>
2023-10-05 13:04:17 -0400

    updated

--- subnet/main.tf
+++ subnet/main.tf
@@ -7,3 +7,24 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "vpc_id" {
+ default = "vpc-0aa883d9f3240128a"
+}
+
+data "aws_vpc" "main" {
+  id = var.vpc_id
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = data.aws_vpc.main.id
+  cidr_block = cidrsubnet(data.aws_vpc.main.cidr_block, 4, 1)
+
+  tags = {
+    Name = "Main"
+  }
+}
