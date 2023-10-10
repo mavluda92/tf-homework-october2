96de97ae8c0488b268575f6ed8af6230c02e59bf
Mavluda <mavluda.kambarova@gmail.com>
2023-10-10 00:23:49 -0400

    Added billing resources

---
+++ biling/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  cloud {
+    organization = "summer-cloud-2023"
+
+    workspaces {
+      name = "infra-billing"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+module "billing_alert" {
+  source = "binbashar/cost-billing-alarm/aws"
+
+  aws_env = "ziyotek"
+  monthly_billing_threshold = 30
+  currency = "USD"
+  create_sns_topic = true
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = module.billing_alert.sns_topic_arns[0]
+  protocol  = "email"
+  endpoint  = "mavluda.kambarova@gmail.com"
+}
+
